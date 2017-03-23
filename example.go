35ab0dff50861f3bc0e15be7d219258759299363
Vladimir Varankin <nek.narqo+git@gmail.com>
2017-03-23 23:50:22 +0300

    draft query api

--- example.go
+++ example.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 
 	"github.com/narqo/yoctodb"
@@ -14,7 +15,39 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	if _, err := yoctodb.ReadVerifyDB(bytes.NewReader(dbData)); err != nil {
+	db, err := yoctodb.ReadVerifyDB(bytes.NewReader(dbData))
+	if err != nil {
 		panic(err)
 	}
+
+	/*
+	// Filter the second document
+	final Query doc2 = select().where(eq("id", from(2)));
+	assertTrue(db.count(doc2) == 1);
+	 */
+	doc2 := &yoctodb.Select{
+		Where: &yoctodb.Eq{"id": 2},
+	}
+	n, err := db.Count(doc2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf("doc count: %d\n", n)
+
+	/*
+	// Filter and sort
+	final Query sorted =
+                select().where(and(gte("id", from(1)), lte("id", from(2))))
+                        .orderBy(desc("score"));
+	db.execute(sorted, ...)
+	 */
+	sorted := &yoctodb.Select{
+		Where: &yoctodb.And{
+			&yoctodb.Gte{"id": 1},
+			&yoctodb.Lte{"id": 1},
+		},
+		OrderBy: &yoctodb.Desc("score"),
+	}
+	//err := db.DoProcess(sorted, func() error {})
+	err := db.Do(sorted)
 }
