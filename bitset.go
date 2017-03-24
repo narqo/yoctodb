57428d9b13d3c65865c9626451bfba1af9ff04af
Vladimir Varankin <nek.narqo+git@gmail.com>
2017-03-24 23:38:48 +0300

    wip! bitSet

--- bitset.go
+++ bitset.go
@@ -3,67 +3,83 @@ package yoctodb
 import "sync"
 
 type BitSet interface {
-	Size() uint32
-	Cardinality() uint32
-	Get(i uint32) bool
-	Set(i uint32)
+	Size() int
+	Cardinality() int
+	Get(i int) bool
+	Set(i int)
 }
 
 // readOnlyOneBitSet is a read-only one BitSet implementation.
-type readOnlyOneBitSet uint32
+type readOnlyOneBitSet int
 
-func (b readOnlyOneBitSet) Size() uint32 {
-	return uint32(b)
+func (b readOnlyOneBitSet) Size() int {
+	return int(b)
 }
 
-func (b readOnlyOneBitSet) Cardinality() uint32 {
-	return uint32(b)
+func (b readOnlyOneBitSet) Cardinality() int {
+	return int(b)
 }
 
-func (b readOnlyOneBitSet) Get(i uint32) bool {
+func (b readOnlyOneBitSet) Get(i int) bool {
 	return true
 }
 
-func (b readOnlyOneBitSet) Set(i uint32) {
+func (b readOnlyOneBitSet) Set(i int) {
 	return
 }
 
 // readOnlyZeroBitSet is a read-only zero BitSet implementation.
-type readOnlyZeroBitSet uint32
+type readOnlyZeroBitSet int
 
-func (b readOnlyZeroBitSet) Size() uint32 {
-	return uint32(b)
+func (b readOnlyZeroBitSet) Size() int {
+	return int(b)
 }
 
-func (b readOnlyZeroBitSet) Cardinality() uint32 {
+func (b readOnlyZeroBitSet) Cardinality() int {
 	return 0
 }
 
-func (b readOnlyZeroBitSet) Get(i uint32) bool {
+func (b readOnlyZeroBitSet) Get(i int) bool {
 	return false
 }
 
-func (b readOnlyZeroBitSet) Set(i uint32) {
+func (b readOnlyZeroBitSet) Set(i int) {
 	return
 }
 
-// bitSet...
-type bitSet []byte
-
-func (b *bitSet) Size() uint32 {
-	return uint32(len(*b))
+// bitSet is a bit array.
+type bitSet struct {
+	l     int
+	words []uint8
 }
 
-func (b *bitSet) Cardinality() uint32 {
+func NewBitSet(l int) BitSet {
+	return &bitSet{l, make([]uint8, 7+l/8)}
+}
+
+func (b *bitSet) Size() int {
+	return b.l
+}
+
+func (b *bitSet) Cardinality() int {
 	panic("implement me")
 }
 
-func (b *bitSet) Get(i uint32) bool {
-	panic("implement me")
+func (b *bitSet) Get(i int) bool {
+	if i >= b.l {
+		return false
+	}
+	word := b.words[i%8]
+	bit := uint8(1 << uint(i))
+	return word&bit != 0
 }
 
-func (b *bitSet) Set(i uint32) {
-	panic("implement me")
+func (b *bitSet) Set(i int) {
+	if i >= b.l {
+		return
+	}
+	bit := uint8(1 << uint(i))
+	b.words[i%8] |= bit
 }
 
 var bitSetPool = sync.Pool{
